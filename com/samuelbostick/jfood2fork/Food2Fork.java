c80227a89e69790e1bea2e974aff60c8a17acdf9
frankenbeanies <samuel.j.bostick@gmail.com>
2016-09-14 16:13:29 -0400

    Refactored Http requests

--- com/samuelbostick/jfood2fork/Food2Fork.java
+++ com/samuelbostick/jfood2fork/Food2Fork.java
@@ -15,40 +15,24 @@ public class Food2Fork {
         _apiKey = apiKey;
     }
 
-    public Recipe getRecipe(int id) throws Exception{
-        HttpURLConnection urlConnection = null;
-        String json = "";
-        try{
-            URL url = new URL("http://food2fork.com/api/get?key=" + _apiKey + "&rId=" + id);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            InputStreamReader reader = new InputStreamReader(url.openStream());
-
-            int d = reader.read();
-            while(d != -1){
-                json += (char)d;
-                d = reader.read();
-            }
-        }catch(MalformedURLException e){
-            throw new Exception("Malformed apiKey or recipeID");
-
-        }finally{
-            if(urlConnection != null){
-                urlConnection.disconnect();
-            }
-        }
-
-        System.out.println(json);
-
+    public Recipe getRecipe(int id) throws IOException{
+        String json = getJsonFromUrl("http://food2fork.com/api/get?key=" + _apiKey + "&rId=" + id);
         Genson genson = new GensonBuilder().useRuntimeType(true).create();
         return genson.deserialize(json, RecipeContainer.class).recipe;
     }
 
-    public SearchResult search(String query, char sort, int page) throws Exception{
+    public SearchResult search(String query, char sort, int page) throws MalformedURLException, IOException{
+        String json = getJsonFromUrl("http://food2fork.com/api/search?key=" + _apiKey
+                + "&q=" + query + "&sort=" + sort + "&page=" + page);
+        Genson genson = new GensonBuilder().useRuntimeType(true).create();
+        return genson.deserialize(json, SearchResult.class);
+    }
+
+    private String getJsonFromUrl(String s) throws IOException{
         HttpURLConnection urlConnection = null;
         String json = "";
         try{
-            URL url = new URL("http://food2fork.com/api/search?key=" + _apiKey
-                + "&q=" + query + "&sort=" + sort + "&page=" + page);
+            URL url = new URL(s);
             urlConnection = (HttpURLConnection) url.openConnection();
             InputStreamReader reader = new InputStreamReader(url.openStream());
 
@@ -57,18 +41,14 @@ public class Food2Fork {
                 json += (char)d;
                 d = reader.read();
             }
-        }catch(MalformedURLException e){
-            throw new Exception("Malformed parameter");
-
+        }catch(IOException e){
+            throw new IOException("There was an error reading from the url. Check your parameters, and that the endpoint is online.");
         }finally{
             if(urlConnection != null){
                 urlConnection.disconnect();
             }
         }
 
-        System.out.println(json);
-
-        Genson genson = new GensonBuilder().useRuntimeType(true).create();
-        return genson.deserialize(json, SearchResult.class);
+        return json;
     }
 }
